5c5f76d9e6b809f8b376723e4f1e9bf82b8634e7
Vượng <24100383@st.phenikaa-uni.edu.vn>
2026-07-24 10:42:57 +0000

    thêm biểu đồ cân nặng

--- lib/screens/health/weight_screen.dart
+++ lib/screens/health/weight_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
 import '../../services/weight_service.dart';
+
 
 
 class WeightScreen extends StatefulWidget {
@@ -8,27 +11,87 @@ class WeightScreen extends StatefulWidget {
 
 
   @override
-  State<WeightScreen> createState() => _WeightScreenState();
+  State<WeightScreen> createState() =>
+      _WeightScreenState();
 
 }
+
+
 
 
 
 class _WeightScreenState extends State<WeightScreen> {
 
 
+
+  List<FlSpot> getChartData(){
+
+
+    List<FlSpot> spots = [];
+
+
+
+    for(int i = 0; i < WeightService.weightList.length; i++){
+
+
+      double weight =
+
+      double.parse(
+
+        WeightService
+            .weightList[i]
+            .weight
+            .replaceAll(" kg", ""),
+
+      );
+
+
+
+      spots.add(
+
+        FlSpot(
+
+          i.toDouble(),
+
+          weight,
+
+        ),
+
+      );
+
+
+    }
+
+
+
+    return spots;
+
+
+  }
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
 
 
-    final weightList = WeightService.weightList;
+    final weightList =
+        WeightService.weightList;
+
 
 
     return Scaffold(
 
-      appBar: AppBar(
 
-        title: const Text(
+      appBar:
+
+      AppBar(
+
+        title:
+        const Text(
           "Danh sách cân nặng",
         ),
 
@@ -36,133 +99,372 @@ class _WeightScreenState extends State<WeightScreen> {
 
 
 
-      body: Padding(
-
-        padding: const EdgeInsets.all(16),
 
 
-        child: Column(
+      body:
 
-          crossAxisAlignment: CrossAxisAlignment.start,
+      Padding(
+
+
+        padding:
+        const EdgeInsets.all(16),
+
+
+
+
+
+        child:
+
+        Column(
+
+
+          crossAxisAlignment:
+          CrossAxisAlignment.start,
+
 
 
           children: [
 
 
+
+
+
             const Text(
 
-              "Lịch sử cân nặng",
 
-              style: TextStyle(
+              "Biểu đồ cân nặng",
 
-                fontSize: 20,
 
-                fontWeight: FontWeight.bold,
+              style:
+
+              TextStyle(
+
+                fontSize:20,
+
+                fontWeight:
+                FontWeight.bold,
 
               ),
+
 
             ),
 
 
 
-            const SizedBox(height: 15),
+
+
+            const SizedBox(height:15),
+
+
+
+
+
+
+            SizedBox(
+
+
+              height:250,
+
+
+
+              child:
+
+              weightList.isEmpty
+
+
+
+                  ?
+
+              const Center(
+
+                child:
+                Text(
+                  "Chưa có dữ liệu",
+                ),
+
+              )
+
+
+
+                  :
+
+              LineChart(
+
+
+                LineChartData(
+
+
+                  gridData:
+
+                  const FlGridData(
+
+                    show:true,
+
+                  ),
+
+
+
+                  titlesData:
+
+                  const FlTitlesData(
+
+                    show:true,
+
+                  ),
+
+
+
+
+                  borderData:
+
+                  FlBorderData(
+
+                    show:true,
+
+                  ),
+
+
+
+
+
+                  lineBarsData: [
+
+
+
+                    LineChartBarData(
+
+
+                      spots:
+                      getChartData(),
+
+
+
+                      isCurved:true,
+
+
+
+                      barWidth:3,
+
+
+
+                      dotData:
+
+                      const FlDotData(
+
+                        show:true,
+
+                      ),
+
+
+
+                    ),
+
+
+
+                  ],
+
+
+
+                ),
+
+
+
+              ),
+
+
+
+            ),
+
+
+
+
+
+            const SizedBox(height:25),
+
+
+
+
+
+
+
+            const Text(
+
+
+              "Lịch sử cân nặng",
+
+
+              style:
+
+              TextStyle(
+
+                fontSize:20,
+
+                fontWeight:
+                FontWeight.bold,
+
+              ),
+
+
+            ),
+
+
+
+
+
+
+            const SizedBox(height:15),
+
+
+
+
 
 
 
             Expanded(
 
 
-              child: weightList.isEmpty
+              child:
+
+              weightList.isEmpty
 
 
-                  ? const Center(
 
-                      child: Text(
-                        "Chưa có dữ liệu cân nặng",
+                  ?
+
+              const Center(
+
+                child:
+                Text(
+                  "Chưa có dữ liệu cân nặng",
+                ),
+
+              )
+
+
+
+                  :
+
+              ListView.builder(
+
+
+
+                itemCount:
+                weightList.length,
+
+
+
+                itemBuilder:
+                    (context,index){
+
+
+
+                  return Card(
+
+
+
+                    child:
+
+                    ListTile(
+
+
+
+                      leading:
+
+                      const Icon(
+
+
+                        Icons.monitor_weight,
+
+
+                        size:35,
+
+
                       ),
 
-                    )
 
 
 
-                  : ListView.builder(
+
+                      title:
+
+                      Text(
 
 
-                      itemCount: weightList.length,
+                        weightList[index].weight,
 
 
+                        style:
 
-                      itemBuilder: (context, index) {
-
-
-
-                        return Card(
+                        const TextStyle(
 
 
-                          child: ListTile(
+                          fontSize:18,
 
 
-                            leading: const Icon(
-
-                              Icons.monitor_weight,
-
-                              size: 35,
-
-                            ),
+                          fontWeight:
+                          FontWeight.bold,
 
 
+                        ),
 
-                            title: Text(
 
-                              weightList[index].weight,
-
-                              style: const TextStyle(
-
-                                fontSize: 18,
-
-                                fontWeight: FontWeight.bold,
-
-                              ),
-
-                            ),
+                      ),
 
 
 
-                            subtitle: Text(
-
-                              "Ngày cân: ${weightList[index].date}",
-
-                            ),
 
 
 
-                          ),
+                      subtitle:
+
+                      Text(
 
 
-                        );
+                        "Ngày cân: ${weightList[index].date}",
 
 
-                      },
+                      ),
+
+
+
+
 
                     ),
+
+
+
+                  );
+
+
+
+                },
+
+
+              ),
+
 
 
             ),
 
 
+
           ],
+
 
 
         ),
 
 
+
       ),
+
 
 
     );
 
 
   }
+
 
 
 }
