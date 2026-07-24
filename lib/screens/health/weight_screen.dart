fb8ac517c3f209fb8fbd696148b73a56427182a2
Vượng <24100383@st.phenikaa-uni.edu.vn>
2026-07-24 09:57:13 +0000

    Create weight history screen

--- lib/screens/health/weight_screen.dart
+++ lib/screens/health/weight_screen.dart
@@ -1,135 +1,155 @@
 import 'package:flutter/material.dart';
 
+
 class WeightScreen extends StatelessWidget {
+
   const WeightScreen({super.key});
+
+
+  // Dữ liệu tạm
+  final List<Map<String, String>> weightList = const [
+
+    {
+      "date": "01/06/2026",
+      "weight": "10.5 kg",
+    },
+
+    {
+      "date": "15/06/2026",
+      "weight": "11.2 kg",
+    },
+
+    {
+      "date": "01/07/2026",
+      "weight": "12.0 kg",
+    },
+
+    {
+      "date": "27/07/2026",
+      "weight": "12.5 kg",
+    },
+
+  ];
+
+
 
   @override
   Widget build(BuildContext context) {
 
+
     return Scaffold(
 
       appBar: AppBar(
+
         title: const Text(
           "Danh sách cân nặng",
         ),
+
       ),
 
 
+
       body: Padding(
+
         padding: const EdgeInsets.all(16),
 
+
+
         child: Column(
+
           crossAxisAlignment: CrossAxisAlignment.start,
 
           children: [
 
+
+
             const Text(
+
               "Lịch sử cân nặng",
+
               style: TextStyle(
+
                 fontSize: 20,
+
                 fontWeight: FontWeight.bold,
-              ),
-            ),
-
-
-            const SizedBox(height: 20),
-
-
-            Card(
-              child: ListTile(
-
-                leading: const Icon(
-                  Icons.monitor_weight,
-                ),
-
-                title: const Text(
-                  "01/07/2026",
-                ),
-
-                subtitle: const Text(
-                  "12.5 kg",
-                ),
-
-              ),
-            ),
-
-
-            Card(
-              child: ListTile(
-
-                leading: const Icon(
-                  Icons.monitor_weight,
-                ),
-
-                title: const Text(
-                  "01/06/2026",
-                ),
-
-                subtitle: const Text(
-                  "11.8 kg",
-                ),
-
-              ),
-            ),
-
-
-            Card(
-              child: ListTile(
-
-                leading: const Icon(
-                  Icons.monitor_weight,
-                ),
-
-                title: const Text(
-                  "01/05/2026",
-                ),
-
-                subtitle: const Text(
-                  "11.2 kg",
-                ),
-
-              ),
-            ),
-
-
-            const SizedBox(height: 20),
-
-
-            const Text(
-              "Biểu đồ cân nặng",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-
-            const SizedBox(height: 10),
-
-
-            Container(
-              height: 150,
-              width: double.infinity,
-
-              decoration: BoxDecoration(
-
-                border: Border.all(),
-
-                borderRadius: BorderRadius.circular(10),
-
-              ),
-
-              child: const Center(
-
-                child: Text(
-                  "Biểu đồ cân nặng",
-                ),
 
               ),
 
             ),
+
+
+
+            const SizedBox(height: 15),
+
+
+
+
+
+            Expanded(
+
+              child: ListView.builder(
+
+                itemCount: weightList.length,
+
+
+
+                itemBuilder: (context, index) {
+
+
+                  return Card(
+
+
+                    child: ListTile(
+
+
+                      leading: const Icon(
+
+                        Icons.monitor_weight,
+
+                        size: 35,
+
+                      ),
+
+
+
+                      title: Text(
+
+                        weightList[index]["weight"]!,
+
+                        style: const TextStyle(
+
+                          fontSize: 18,
+
+                          fontWeight: FontWeight.bold,
+
+                        ),
+
+                      ),
+
+
+
+                      subtitle: Text(
+
+                        "Ngày cân: ${weightList[index]["date"]}",
+
+                      ),
+
+
+
+                    ),
+
+                  );
+
+
+                },
+
+              ),
+
+            ),
+
 
           ],
+
         ),
 
       ),
@@ -137,4 +157,5 @@ class WeightScreen extends StatelessWidget {
     );
 
   }
+
 }
