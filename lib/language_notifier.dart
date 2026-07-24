0894f3e194bc3c30c95b348b1fa4afe3d1b06550
nguyenvannhatminh <24100084@st.phenikaa-uni.edu.vn>
2026-07-24 20:19:24 +0000

    feat: Thêm languageNotifier quản lý trạng thái ngôn ngữ toàn app

---
+++ lib/language_notifier.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// Quản lý trạng thái ngôn ngữ toàn app ('Tiếng Việt' hoặc 'English')
+final ValueNotifier<String> languageNotifier = ValueNotifier<String>('Tiếng Việt');
