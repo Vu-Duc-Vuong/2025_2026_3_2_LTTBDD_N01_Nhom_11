27e2bba26d2e260bc566fbc3dc46db73dfbf2f86
Vượng <24100383@st.phenikaa-uni.edu.vn>
2026-07-24 09:33:36 +0000

    Create pet profile information screen

--- lib/screens/health/pet_profile_screen.dart
+++ lib/screens/health/pet_profile_screen.dart
@@ -5,7 +5,6 @@ class PetProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
 
       appBar: AppBar(
@@ -14,12 +13,128 @@ class PetProfileScreen extends StatelessWidget {
         ),
       ),
 
-      body: const Center(
-        child: Text(
-          "Thông tin hồ sơ thú cưng",
-          style: TextStyle(
-            fontSize: 20,
-          ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            // Ảnh thú cưng
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+
+                child: const Icon(
+                  Icons.pets,
+                  size: 60,
+                ),
+              ),
+            ),
+
+
+            const SizedBox(height: 20),
+
+
+            const Card(
+              child: ListTile(
+
+                leading: Icon(
+                  Icons.pets,
+                ),
+
+                title: Text(
+                  "Tên thú cưng",
+                ),
+
+                subtitle: Text(
+                  "Mít",
+                ),
+
+              ),
+            ),
+
+
+            const Card(
+              child: ListTile(
+
+                leading: Icon(
+                  Icons.category,
+                ),
+
+                title: Text(
+                  "Giống loài",
+                ),
+
+                subtitle: Text(
+                  "Golden Retriever",
+                ),
+
+              ),
+            ),
+
+
+            const Card(
+              child: ListTile(
+
+                leading: Icon(
+                  Icons.cake,
+                ),
+
+                title: Text(
+                  "Tuổi",
+                ),
+
+                subtitle: Text(
+                  "2 tuổi",
+                ),
+
+              ),
+            ),
+
+
+            const Card(
+              child: ListTile(
+
+                leading: Icon(
+                  Icons.male,
+                ),
+
+                title: Text(
+                  "Giới tính",
+                ),
+
+                subtitle: Text(
+                  "Đực",
+                ),
+
+              ),
+            ),
+
+
+            const Card(
+              child: ListTile(
+
+                leading: Icon(
+                  Icons.notes,
+                ),
+
+                title: Text(
+                  "Ghi chú",
+                ),
+
+                subtitle: Text(
+                  "Thú cưng khỏe mạnh",
+
+                ),
+
+              ),
+            ),
+
+
+          ],
         ),
       ),
 
