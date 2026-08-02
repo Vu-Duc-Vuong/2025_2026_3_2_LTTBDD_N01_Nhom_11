9f67e0e564e89fccb3cd9fcf35dac98ea4511ca0
Vu-Duc-Vuong <24100383@st.phenikaa-uni.edu.vn>
2026-08-02 17:54:42 +0700

    Fix weight history by pet id

--- lib/services/weight_service.dart
+++ lib/services/weight_service.dart
@@ -7,21 +7,21 @@ class WeightService {
   static final List<WeightModel> weightList = [
 
     WeightModel(
-      petId: "1",
+      petId: "1", // Lucky
       date: "01/06/2026",
-      weight: "11.5 kg",
+      weight: "3.5 kg",
     ),
 
     WeightModel(
       petId: "1",
       date: "15/06/2026",
-      weight: "12 kg",
+      weight: "3.6 kg",
     ),
 
     WeightModel(
-      petId: "2",
-      date: "01/07/2026",
-      weight: "12.5 kg",
+      petId: "2", // Milo
+      date: "01/06/2026",
+      weight: "4.2 kg",
     ),
 
   ];
